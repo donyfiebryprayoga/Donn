d2ecbb6a1dd0ef225fc2e3c8f010810bb73a48ff
donyfiebryprayoga <donyfiebryprayoga@gmail.com>
2026-09-03 15:16:24 +0700

    Change repository path in Games table
    Updated the repository path for game scripts.

--- List Game/List-Game.lua
+++ List Game/List-Game.lua
@@ -1,8 +1,8 @@
 local Games = {
-    [126884695634066] = "https://raw.githubusercontent.com/donyfiebryprayoga/DonnHUB/refs/heads/main/Game%20Roblox/GrowAGarden.lua",
-    [97598239454123] = "https://raw.githubusercontent.com/donyfiebryprayoga/DonnHUB/refs/heads/main/Game%20Roblox/GrowAGarden2.lua",
-    [121864768012064] = "https://raw.githubusercontent.com/donyfiebryprayoga/DonnHUB/refs/heads/main/Game%20Roblox/Fishtit.lua",
-    [107778070777162] = "https://raw.githubusercontent.com/donyfiebryprayoga/DonnHUB/refs/heads/main/Game%20Roblox/StealAnEgg.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/Game%20Roblox/GrowAGarden.lua",
+    [97598239454123] = "https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/Game%20Roblox/GrowAGarden2.lua",
+    [121864768012064] = "https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/Game%20Roblox/Fishtit.lua",
+    [107778070777162] = "https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/Game%20Roblox/StealAnEgg.lua",
 }
 
 return Games

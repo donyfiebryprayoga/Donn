c39535083b06d68a68eb88e093c873a225308973
donyfiebryprayoga <donyfiebryprayoga@gmail.com>
2026-09-03 15:40:31 +0700

    Refactor game loading logic in Donnhub.lua

--- loader/Donnhub.lua
+++ loader/Donnhub.lua
@@ -1,25 +1,19 @@
+local HttpGet = game.HttpGet
 local CurrentPlaceId = game.PlaceId
 
 local Success, Games = pcall(function()
-    local response = game:HttpGet("https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/List%20Game/List-Game.lua")
-    return loadstring(response)()
+    return loadstring(HttpGet(game, "https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/List%20Game/List-Game.lua"))()
 end)
 
 if not Success or type(Games) ~= "table" then 
-    warn("[DonnHub] Gagal memuat list game!")
+    warn("Gagal memuat list game kaitun!")
     return 
 end
 
 local URL = Games[CurrentPlaceId]
 if not URL then 
-    warn("[DonnHub] Game ini tidak didukung.")
+    warn("Game tidak didukung di DonnHub Kaitun.")
     return 
 end
 
-local SuccessLoad, Result = pcall(function()
-    return loadstring(game:HttpGet(URL))()
-end)
-
-if not SuccessLoad then
-    warn("[DonnHub] Gagal menjalankan skrip untuk game ini: " .. tostring(Result))
-end
+loadstring(HttpGet(game, URL))()

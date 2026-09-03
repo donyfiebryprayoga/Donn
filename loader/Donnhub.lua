435fad34daaf0c4ba492aeae61d54fcc0b5c1f48
donyfiebryprayoga <donyfiebryprayoga@gmail.com>
2026-09-03 15:20:18 +0700

    Improve game loading process in Donnhub.lua
    Enhanced error handling and logging for game loading.

--- loader/Donnhub.lua
+++ loader/Donnhub.lua
@@ -1,19 +1,25 @@
-local HttpGet = game.HttpGet
 local CurrentPlaceId = game.PlaceId
 
 local Success, Games = pcall(function()
-    return loadstring(HttpGet(game, "https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/List%20Game/List-Game.lua"))()
+    local response = game:HttpGet("https://raw.githubusercontent.com/donyfiebryprayoga/Donn/refs/heads/main/List%20Game/List-Game.lua")
+    return loadstring(response)()
 end)
 
 if not Success or type(Games) ~= "table" then 
-    warn("Gagal memuat list game normal!")
+    warn("[DonnHub] Gagal memuat list game!")
     return 
 end
 
 local URL = Games[CurrentPlaceId]
 if not URL then 
-    warn("Game tidak didukung di DonnHub Normal.")
+    warn("[DonnHub] Game ini tidak didukung.")
     return 
 end
 
-loadstring(HttpGet(game, URL))()
+local SuccessLoad, Result = pcall(function()
+    return loadstring(game:HttpGet(URL))()
+end)
+
+if not SuccessLoad then
+    warn("[DonnHub] Gagal menjalankan skrip untuk game ini: " .. tostring(Result))
+end
